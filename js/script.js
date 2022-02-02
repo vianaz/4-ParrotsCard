f436c615fdaffa57ceea00ed7ae975627ea5fec5
Gabriel Viana Boaes <viana_gab2001@hotmail.com>
2022-02-02 10:25:06 -0300

    commit de fim de trabalho, estou começando a implemntar a aleatoriedade dos pares

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
 const gridCards = document.createElement('div');
 gridCards.classList.add('grid-cartas');
-const imagensVerso = ['bobrossparrot.gif','explodyparrot.gif','fiestaparrot.gif','metalparrot.gif','revertitparrot.gif', 'tripletsparrot.gif','unicornparrot.gif'];
+let imagensVerso = ['bobrossparrot.gif','explodyparrot.gif','fiestaparrot.gif','metalparrot.gif','revertitparrot.gif', 'tripletsparrot.gif','unicornparrot.gif'];
 
-perguntarCartas();
 // Prompt perguntando sobre o numero de cartas
 function perguntarCartas() {
     let numeroCartas = 0;
@@ -12,36 +11,75 @@ function perguntarCartas() {
             alert('Valor invalido!');
         }
     }
+    return numeroCartas;
 }
-// Cria a carta
-function criarCarta() {
-    // Declara as divs da Carta
+
+// Função que deixa misturada a Array
+function shuffle(array) {
+    let m = array.length;
+    while (m) {
+    // Pick a remaining element…
+        i = Math.floor(Math.random() * m--);
+    // And swap it with the current element.
+        t = array[m];
+        array[m] = array[i];
+        array[i] = t;
+    }
+    return array;
+}
+
+// Pega no numero de cartas desejado, adiciona em pares e mistura
+function estruturarPares(numero) {
+    const cartasEmJogo = [];   
+    for (let i = 0; i < numero; i++) {
+        cartasEmJogo.push(imagensVerso[i]);
+        cartasEmJogo.push(imagensVerso[i]);
+    }
+    shuffle(cartasEmJogo);
+    return cartasEmJogo;
+}
+
+// Montar a estrutura das cartas
+function montarCarta() {
     const conteinerCarta = document.createElement('div');
     const frontFace = document.createElement('div');
     const imgFront = document.createElement('img');
     const backFace = document.createElement('div');
     const imgBack = document.createElement('img');
+    
 
     // Adiciona as Classes necessárias
     conteinerCarta.classList.add('card');
     frontFace.classList.add('front-face', 'face');
     backFace.classList.add('back-face', 'face');
     imgFront.src = 'img/front.png';
-    imgBack.src = `img/${imagensVerso[shuffle()]}`;
+    // imgBack.src = `img/${}`;
 
-    // Organiza a estrutura HTML da carta e Adiciona ao HTML
+}
+// Organiza a estrutura HTML da carta e Adiciona ao HTML
+function montarHTML() {
     conteinerCarta.appendChild(frontFace);
     conteinerCarta.appendChild(backFace);
     frontFace.appendChild(imgFront);
     backFace.appendChild(imgBack);
-    
+
     gridCards.appendChild(conteinerCarta);
     document.body.appendChild(gridCards);
 }
 
-// Função que deixa aleatorio a Array
-function shuffle() {
-    let valor = Math.floor(Math.random() * 7);
-    console.log(valor);
-    return valor;
+
+// Vai pegar o número de cartas e colocar em jogo
+function colocarCartasJogo() {
+    let teste = perguntarCartas();
+    for (let i = 0; i < teste; i++) {
+        montarCarta();
+    }
 }
+
+// Função que exectura o jogo
+function executarJogo() {
+    const paresCartas = perguntarCartas() / 2;
+    shuffle(imagensVerso);
+    estruturarPares(paresCartas);
+}
+executarJogo();
