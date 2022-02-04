c257bb58fce35175bcb813260bebeae81637a38b
Gabriel Viana Boaes <viana_gab2001@hotmail.com>
2022-02-04 15:09:17 -0300

    Jogo teoricamente finalizado, só falta os bonus e dar uma revisada no codigo para melhorias

--- js/script.js
+++ js/script.js
@@ -1,19 +1,22 @@
 // Variaveis globais necessárias
-let imagensVerso = ['bobrossparrot.gif','explodyparrot.gif','fiestaparrot.gif','metalparrot.gif','revertitparrot.gif', 'tripletsparrot.gif','unicornparrot.gif'];
-const cartasEmJogo = [];  
-let numeroCartas = 0;
+let listaImagensVerso = ['bobrossparrot','explodyparrot','fiestaparrot','metalparrot','revertitparrot', 'tripletsparrot','unicornparrot'];
+const listaCartasEmJogo = [];  
+let numeroDeCartasDesejado = 0;
+let listaDePares = [];
+let contadorDeJogads = 0;
+let contadorFinalizarJogo = 0;
 
 
 // Prompt perguntando sobre o numero de cartas
 function perguntarCartas() {
     alert('É um jogo da memória, então tente achar as cartas certas! :)')
-    while (numeroCartas < 4 || numeroCartas > 14 || numeroCartas % 2 != 0) {
-        numeroCartas = prompt('Quantas cartas você deseja?(números pares de 4 a 14)');
-        if (numeroCartas < 4 || numeroCartas > 14 || numeroCartas % 2 != 0) {
+    while (numeroDeCartasDesejado < 4 || numeroDeCartasDesejado > 14 || numeroDeCartasDesejado % 2 != 0) {
+        numeroDeCartasDesejado = prompt('Quantas cartas você deseja?(números pares de 4 a 14)');
+        if (numeroDeCartasDesejado < 4 || numeroDeCartasDesejado > 14 || numeroDeCartasDesejado % 2 != 0) {
             alert('Valor invalido!');
         }
     }
-    return numeroCartas;
+    return numeroDeCartasDesejado;
 }
 
 // Função que deixa misturada a Array
@@ -33,44 +36,89 @@ function shuffle(array) {
 // Pega no numero de cartas desejado, adiciona em pares e mistura
 function estruturarPares(valor) {
     for (let i = 0; i < valor; i++) {
-        cartasEmJogo.push(imagensVerso[i]);
-        cartasEmJogo.push(imagensVerso[i]);
+        listaCartasEmJogo.push(listaImagensVerso[i]);
+        listaCartasEmJogo.push(listaImagensVerso[i]);
     }
-    shuffle(cartasEmJogo);
+    shuffle(listaCartasEmJogo);
 } 
 
 // Montar a estrutura das cartas
 function montarCarta(numero) {
     let gridCartas = document.querySelector('div');
     let cartas = `
-    <div class="card" onclick="girarCartas(this)">
+    <div class="card" onclick="girarCartas(this);verificarParesIguais(this)" id="${listaCartasEmJogo[numero]}">
         <div class="front-face face">
             <img src="img/front.png">
         </div>
         <div class="back-face face">
-            <img src="img/${cartasEmJogo[numero]}">
+            <img src="img/${listaCartasEmJogo[numero]}.gif">
         </div>
     </div>`;
     gridCartas.innerHTML = gridCartas.innerHTML + cartas;
 }
 
+// Faz a carta girar para mostrar o verso
 function girarCartas(carta) {
     let frontFace = carta.children[0];
     let backFace = carta.children[1];
 
-    frontFace.classList.toggle('front-face');
-    frontFace.classList.toggle('girar-tras');
-    backFace.classList.toggle('back-face');
-    backFace.classList.toggle('girar-frente');
+    frontFace.classList.remove('front-face');
+    frontFace.classList.add('girar-tras');
+    backFace.classList.remove('back-face');
+    backFace.classList.add('girar-frente');
+    contadorDeJogads ++;
+
+}
+
+// Faz a carta desgirar para desmotrar o verso
+function desgirarCartas() {
+    for (let i = 0; i < listaDePares.length; i++) {
+        let frontFace = listaDePares[i].children[0];
+        let backFace = listaDePares[i].children[1];
+        
+        frontFace.classList.remove('girar-tras');
+        frontFace.classList.add('front-face');
+        backFace.classList.remove('girar-frente');
+        backFace.classList.add('back-face');
+    }
+    listaDePares = []
+} 
+
+// Verificar cartas iguais
+function verificarParesIguais(escolhida) {
+    listaDePares.push(escolhida);
+    if (listaDePares.length === 2) {
+        if (listaDePares[0].id === listaDePares[1].id) {
+            contadorFinalizarJogo++;
+            listaDePares = [];
+            finalizarJogo();
+        } else {
+            setTimeout(desgirarCartas, 1000);
+        }
+    } 
+}
+
+// Verifica se o jogo já terminou
+function finalizarJogo() {
+    if (contadorFinalizarJogo === (numeroDeCartasDesejado / 2)) {
+        alert(`Você ganhou em ${contadorDeJogads} jogadas!`);
+    }
 }
 
 // Função que exectura o jogo
 function executarJogo() {
+    // Define quantos pares irá ter
     const paresCartas = perguntarCartas() / 2;
-    shuffle(imagensVerso);
+
+    // Mistura as back-faces possíveis
+    shuffle(listaImagensVerso);
+
+    // Constroi os pares de carta, mistura e coloca eles em jogo
     estruturarPares(paresCartas);
-    for (let i = 0; i < numeroCartas; i++) {
+    for (let i = 0; i < numeroDeCartasDesejado; i++) {
         montarCarta(i);
     }
+
+
 }
 executarJogo()
